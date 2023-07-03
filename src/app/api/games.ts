0cf48e2e5de660d99b2fe07995c37407877c344d
rickybrce <rikardo.komen@inet.hr>
2023-07-03 20:47:21 +0200

    api games

--- src/app/api/games.ts
+++ src/app/api/games.ts
@@ -62,3 +62,104 @@ export function getGame(id: any): any {
     auth: true
   });
 }
+
+export function updateGame({
+  id,
+  name,
+  open_date,
+  close_date,
+  bets_soft_close,
+  bets_hard_close,
+  enter_amount,
+  remove_bet_amount,
+  bet_late_amount,
+  extra_coins_amount,
+  extra_lev_amount,
+  reveal_bet_amount,
+  category1_name,
+  category2_name,
+  category3_name,
+  category4_name
+}) {
+  const api = new Api();
+  return api.request({
+    endpoint: "games/"+id,
+    type: "PUT",
+    auth: true,
+    body: {
+      name,
+      open_date,
+      close_date,
+      bets_soft_close,
+      bets_hard_close,
+      enter_amount,
+      remove_bet_amount,
+      bet_late_amount,
+      extra_coins_amount,
+      extra_lev_amount,
+      reveal_bet_amount,
+      category1_name,
+      category2_name,
+      category3_name,
+      category4_name
+    },
+  });
+}
+
+export function deleteGame({
+  id
+}) {
+  const api = new Api();
+  return api.request({
+    endpoint: "games/"+id,
+    type: "DELETE",
+    auth: true,
+    body: {
+      id
+    },
+  });
+}
+
+export function createCoin({
+  game_id
+}) {
+  const api = new Api();
+  return api.request({
+    endpoint: "games/"+game_id+"/coin",
+    type: "POST",
+    auth: true,
+    body: {
+      game_id
+    },
+  });
+}
+
+export function enterGame({
+  game_id,
+  character_id
+}) {
+  const api = new Api();
+  return api.request({
+    endpoint: "games/enter/"+game_id+"/"+character_id,
+    type: "POST",
+    auth: true,
+    body: {
+      game_id
+    },
+  });
+}
+
+export function buyExtraCoins({
+  game_id,
+  user_game_id
+}) {
+  const api = new Api();
+  return api.request({
+    endpoint: "games/buy_extra_coins/"+game_id+"?user_game_id="+user_game_id    ,
+    type: "POST",
+    auth: true,
+    body: {
+      game_id
+    },
+  });
+}
